5d2d2900b835ed5540886bfbe48bd423a2f4041a
DaniMMor <35082525+DaniMMor@users.noreply.github.com>
2018-09-09 20:28:34 -0300

    Trat

--- Project1.dpr
+++ Project1.dpr
@@ -2,7 +2,8 @@ program Project1;
 
 uses
   Vcl.Forms,
-  Unit1 in 'Unit1.pas' {Form1};
+  Unit1 in 'Unit1.pas' {Form1},
+  TratException in 'TratException.pas';
 
 {$R *.res}
 
---
+++ TratException.pas
@@ -0,0 +1,7 @@
+unit TratException;
+
+interface
+
+implementation
+
+end.
